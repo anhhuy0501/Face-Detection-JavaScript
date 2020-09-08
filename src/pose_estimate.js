f8bc89bd74d98af88cefab323ed4dcf345272f6a
Huy-desktop <anhhuy0501@gmail.com>
2020-09-08 16:41:01 +0900

    clean up + add face expression

--- src/pose_estimate.js
+++ src/pose_estimate.js
@@ -8,12 +8,12 @@ const kf3 = new KalmanFilter({R: 1, Q: 10});
 const STRAIGHT_POSE = [0, 0, 0]
 const LEFT_POSE = [-1, 0, 0]
 const RIGHT_POSE = [1, 0, 0]
-const POSE_STRAIGHT_ABS_TOL = 0.12
-const POSE_TURN_ABS_TOL = 0.2
+const POSE_STRAIGHT_ABS_TOL = 0.2
+const POSE_TURN_ABS_TOL = 0.1
 const REL_TOL = 0.05
 const OVAL_LTRB = [250, 90, 380, 220]
 const POSE_STABLE_TOL = 0.3
-const FACE_BOX_ABS_TOL = 10
+const FACE_BOX_ABS_TOL = 20
 
 // Calib
 const MAX_LEFT_POSE = 16
@@ -28,50 +28,57 @@ const init=function(video){
     return [canvas,displaySize]
 }
 
+const detect_expression = function(resizedDetections){
+    const expr = resizedDetections[0].expressions;
+    const keys   = Object.keys(expr);
+    const highest_score = Math.max.apply(null, keys.map(function(x) { return expr[x]} ));
+    const highest_expression = keys.filter(function(y) { return expr[y] === highest_score });
+    console.log(highest_expression+ ":" + highest_score);
+    return [highest_expression,highest_score];
+}
+
 const detect_pose = async function(video,canvas,displaySize,oval){
     const detections = await faceapi.detectAllFaces(video,
-        new faceapi.SsdMobilenetv1Options()).withFaceLandmarks()
-    console.log(detections)
-    const resizedDetections = faceapi.resizeResults(detections,displaySize)
-    canvas.getContext('2d').clearRect(0,0,canvas.width,canvas.height)
-    faceapi.draw.drawDetections(canvas,resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas,resizedDetections)
-
+        new faceapi.SsdMobilenetv1Options()).withFaceLandmarks().withFaceExpressions();
+    //console.log(detections)
+    const resizedDetections = faceapi.resizeResults(detections,displaySize);
+    canvas.getContext('2d').clearRect(0,0,canvas.width,canvas.height);
+    //faceapi.draw.drawDetections(canvas,resizedDetections)
+    //faceapi.draw.drawFaceLandmarks(canvas,resizedDetections)
+    
     
     // Get face box:
     const face_box = {
-        x : detections[0].alignedRect.box.x,
-        y : detections[0].alignedRect.box.y,
-        w : detections[0].alignedRect.box.width,
-        h : detections[0].alignedRect.box.height,
+        x : resizedDetections[0].detection.box.x,
+        y : resizedDetections[0].detection.box.y,
+        w : resizedDetections[0].detection.box.width,
+        h : resizedDetections[0].detection.box.height,
     }
+    console.log("face_box:" + JSON.stringify(face_box))
 
     // Estimate face pose
     const landmark={
-        nose : getMeanPosition(detections[0].landmarks.getNose()),
-        left_eye : getMeanPosition(detections[0].landmarks.getLeftEye()),
-        right_eye : getMeanPosition(detections[0].landmarks.getRightEye()),
-        chin : getMeanPosition([detections[0].landmarks.getJawOutline()[8]]),
+        nose : getMeanPosition(resizedDetections[0].landmarks.getNose()),
+        left_eye : getMeanPosition(resizedDetections[0].landmarks.getLeftEye()),
+        right_eye : getMeanPosition(resizedDetections[0].landmarks.getRightEye()),
+        chin : getMeanPosition([resizedDetections[0].landmarks.getJawOutline()[8]]),
     };
     // Get pose
     //const pose = pose_estimate(landmark, video.width, video.height);
     const pose = simple_pose_estimate(landmark,face_box)
+
     // Get conclude
-    const result = get_conclude(pose, face_box, oval);
-    
+    let result = get_conclude(pose, face_box, oval);
+
+    //Get expression
+    let highest_expression,highest_score;
+    [highest_expression,highest_score] = detect_expression(resizedDetections);
+    result = highest_expression + "_" + result;
+
     console.log(result)
-
+    return result
 }
 
-// Find Landmark from video example
-/*
-const detect_landmark_and_pose = function(video) {
-    let canvas,displaySize
-    [canvas,displaySize] = init(video);
-
-    setInterval(function() {detect_pose(video,canvas,displaySize)},100);
-}
-*/
 
 const approxeq = function(v1, v2, abs_tol,rel_tol) {
     if (abs_tol !== null) {
@@ -91,11 +98,11 @@ function is_straight_face(pose){
 }
 
 function is_turn_left_face(pose){
-    return approxeq(pose[0], LEFT_POSE[0],POSE_TURN_ABS_TOL)
+    return approxeq(pose[0], LEFT_POSE[0],POSE_TURN_ABS_TOL) || (pose[0] < LEFT_POSE[0])
 }
 
 function is_turn_right_face(pose){
-    return approxeq(pose[0], RIGHT_POSE[0],POSE_TURN_ABS_TOL)
+    return approxeq(pose[0], RIGHT_POSE[0],POSE_TURN_ABS_TOL) || (pose[0] > RIGHT_POSE[0])
 }
 
 function is_rect_fix_oval(rec, oval){
@@ -115,20 +122,29 @@ function is_straight_and_in_oval(pose, face_box, oval){
 }
 
 const get_conclude = function(pose, face_box, oval=null) {
-    let conclusion;
-    if (oval && is_straight_and_in_oval(pose, face_box, oval))
-        conclusion = "straight_and_in_oval";
-    else if (oval && is_rect_fix_oval(face_box,oval))
-        conclusion = "in_oval";
-    else if (is_straight_face(pose))
-        conclusion = "straight";
+
+    let posi_text = "";
+    let pose_text = "";
+    let conclusion = "";
+    if (oval && is_rect_fix_oval(face_box,oval))
+        posi_text = "in_oval"; 
+
+    if (is_straight_face(pose))
+        pose_text = conclusion+"straight";
     else if (is_turn_left_face(pose))
-        conclusion = "turn_left"
+        pose_text = "turn_left"
     else if (is_turn_right_face(pose))
-        conclusion = "turn_right"
-    else
-        conclusion = "unknown";
-    return conclusion
+        pose_text = "turn_right"
+    
+    if (pose_text && posi_text)
+        conclusion = pose_text + "_"  + posi_text;
+    else if (pose_text)
+        conclusion = pose_text;
+    else 
+        conclusion = posi_text;
+
+        
+    return conclusion 
 }
 
 
@@ -142,23 +158,7 @@ function getMeanPosition(ls) {
         y: ls[1],
     }
 }
-/*
-function yawpitchrolldecomposition(R){
-    sin_x    = Math.sqrt(R[2,0] * R[2,0] +  R[2,1] * R[2,1])    
-    validity  = sin_x < 1e-6
-    if (!validity ){
-        z1    = Math.atan2(R[2,0], R[2,1])     // around z1-axis
-        x      = Math.atan2(sin_x,  R[2,2])    // around x-axis
-        z2    = Math.atan2(R[0,2], -R[1,2])    // around z2-axis
-    }
-    else{
-        z1    = 0                               // around z1-axis
-        x      = math.atan2(sin_x,  R[2,2])     // around x-axis
-        z2    = 0                               // around z2-axis
-    }
-    return [z1, x, z2]
-}
-*/
+
 const simple_pose_estimate = function (landmark,face_box) {
     //Get middle point
     const middle = face_box.x + face_box.w/2;
@@ -171,148 +171,6 @@ const simple_pose_estimate = function (landmark,face_box) {
     return [yaw,pitch,roll];
 }
 
-/*
-// Estimate pose from landmark
-const pose_estimate = function (landmark,width=720,height=405) {
-    // 3D model points
-    const numRows = 4;
-    const modelPoints = cv.matFromArray(numRows, 3, cv.CV_64FC1, [
-    0.0,
-    0.0,
-    0.0, // Nose tip
-    //0.0,
-    //0.0,
-    //0.0, // HACK! solvePnP doesn't work with 3 points, so copied the
-    //   first point to make the input 4 points
-    0.0, -330.0, -65.0,  // Chin
-    -225.0,
-    170.0,
-    -135.0, // Left eye left corner
-    225.0,
-    170.0,
-    -135.0 // Right eye right corne
-    // -150.0, -150.0, -125.0,  // Left Mouth corner
-    // 150.0, -150.0, -125.0,  // Right mouth corner
-    ]);
-
-    // Camera internals
-    const size = { width: width, height: height };
-    const focalLength = size.width;
-    const center = [size.width / 2, size.height / 2];
-    const cameraMatrix = cv.matFromArray(3, 3, cv.CV_64FC1, [
-        focalLength,
-        0,
-        center[0],
-        0,
-        focalLength,
-        center[1],
-        0,
-        0,
-        1
-    ]);
-    //console.log("Camera Matrix:", cameraMatrix.data64F);
-
-    // Create Matrixes
-    const imagePoints = cv.Mat.zeros(numRows, 2, cv.CV_64FC1);
-    const distCoeffs = cv.Mat.zeros(4, 1, cv.CV_64FC1); // Assuming no lens distortion
-    const rvec = new cv.Mat({ width: 1, height: 3 }, cv.CV_64FC1);
-    const tvec = new cv.Mat({ width: 1, height: 3 }, cv.CV_64FC1);
-    const pointZ = cv.matFromArray(1, 3, cv.CV_64FC1, [0.0, 0.0, 500.0]);
-    const pointY = cv.matFromArray(1, 3, cv.CV_64FC1, [0.0, 500.0, 0.0]);
-    const pointX = cv.matFromArray(1, 3, cv.CV_64FC1, [500.0, 0.0, 0.0]);
-    const noseEndPoint2DZ = new cv.Mat();
-    const nose_end_point2DY = new cv.Mat();
-    const nose_end_point2DX = new cv.Mat();
-    const jaco = new cv.Mat();
-
-    // Finish init
-
-    // Pose estimate
-    const ns = landmark.nose;
-    const le = landmark.left_eye;
-    const re = landmark.right_eye;
-    const chin = landmark.chin;
-    
-    // 2D image points. If you change the image, you need to change vector
-    [
-    ns.x,
-    ns.y, // Nose tip
-    //ns.x,
-    //ns.y, // Nose tip (see HACK! above)
-    chin.x,
-    chin.y,// Chin
-    le.x,
-    le.y, // Left eye left corner
-    re.x,
-    re.y // Right eye right corner
-    // 345, 465, // Left Mouth corner
-    // 453, 469 // Right mouth corner
-    ].map((v, i) => {
-    imagePoints.data64F[i] = v;
-    });  
-    
-    // Hack! initialize transition and rotation matrixes to improve estimation
-    tvec.data64F[0] = -100;
-    tvec.data64F[1] = 100;
-    tvec.data64F[2] = 1000;
-    const distToLeftEyeX = Math.abs(le.x - ns.x);
-    const distToRightEyeX = Math.abs(re.x - ns.x);
-
-    //rvec.data64F[0] = 0;
-    //rvec.data64F[1] = 0;
-    //rvec.data64F[2] = 0;
-
-
-    if (distToLeftEyeX < distToRightEyeX) {
-        // looking at left
-        rvec.data64F[0] = -1.0;
-        rvec.data64F[1] = -0.75;
-        rvec.data64F[2] = -3.0;
-    } else {
-        // looking at right
-        rvec.data64F[0] = 1.0;
-        rvec.data64F[1] = -0.75;
-        rvec.data64F[2] = -3.0;
-    }
-
-    const success = cv.solvePnP(
-    modelPoints,
-    imagePoints,
-    cameraMatrix,
-    distCoeffs,
-    rvec,
-    tvec,
-    true
-    );
-    if (!success) {
-    return;
-    }
-    //console.log("Rotation Vector:", rvec.data64F);
-    //console.log(    "Rotation Vector (in degree):",    rvec.data64F.map(d => (d / Math.PI) * 180));
-
-    // Todo: Need check the ralated of rvec and angle formular again!!!
-    let yaw,pitch,roll
-    [yaw,pitch,roll] = rvec.data64F.map(d => (d / Math.PI) * 180);
-    
-    // Corrected method but not have Rodrigues in js yet.
-    //rmat = cv.Rodrigues(rvec)[0]
-    //yawpitchroll_angles = yawpitchrolldecomposition(rmat).map(d => -180*d/Math.PI);
-    //yawpitchroll_angles[1,0] = yawpitchroll_angles[1,0]+90;
-    //[yaw,pitch,roll] = yawpitchroll_angles;
-    
-
-    if (yaw >= 0){
-        yaw = Math.max(0,180 - yaw )/MAX_RIGHT_POSE;
-    }else{
-        yaw = Math.min(0,-180 -yaw) /MAX_LEFT_POSE;
-    }
-    yaw = kf1.filter(yaw)
-    pitch = kf2.filter(pitch)
-    roll = kf3.filter(roll)
-    console.log([yaw,pitch,roll]);
-    return [yaw,pitch,roll] ;
-}
-*/
 
 const pose_est = {
     init,
--- src/script.js
+++ src/script.js
@@ -8,25 +8,54 @@ Promise.all([
     faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
     faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
 ]).then(startVideo)
 
 function startVideo(){
+    navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => video.srcObject = stream)
+    .then(() => new Promise(resolve => video.onloadedmetadata = resolve))
+
+    
+    /*
     navigator.getUserMedia(
         {video:{}},
         stream => video.srcObject = stream,
         err => console.error(err)
     )
+    */
 }
 startVideo()
 
 const draw_rectangle = function(canvas,rec){
-
-
     //Draw rectangle for testing
     var ctx = canvas.getContext("2d");
     ctx.beginPath();
     ctx.rect(rec.x, rec.y, rec.w, rec.h);
     ctx.stroke();
+}
+
+const draw_text = function(canvas,text,pos){
+    //Draw rectangle for testing
+    var ctx = canvas.getContext("2d");
+    ctx.font = "30px Arial";
+    ctx.fillText(text, pos.x, pos.y);
+    
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRequest(request_actions,current_request) {
+    // Note: will get random request_actions[0]
+    let new_request = request_actions[getRandomInt(1,request_actions.length-1)];
+    while (new_request==current_request){
+        new_request = request_actions[getRandomInt(1,request_actions.length-1)];
+    }
+    return new_request;
 }
 
 video.addEventListener('play',() => {
@@ -35,15 +64,50 @@ video.addEventListener('play',() => {
     
     // Temporary set oval  for testing only
     const oval = {
-        x : 344.4855028167367,
-        y : 143.15230764448643,
-        w : 166.22570246458054,
-        h : 182.8557866513729,
+        x : 300,
+        y : 100,
+        w : 180,
+        h : 200,
     }
+    let result;
+
+    const request_actions = ["happy","in_oval","turn_left","turn_right","straight","surprised","angry"]
+
+    let current_request = request_actions[0];
+    
+    // Count number of request
+    let author_score = 0;
+    const FINISH_SOCRE = 4;
 
     setInterval(function(){
-        pose_est.detect_pose(video,canvas,displaySize,oval);
         draw_rectangle(canvas,oval);
+        result = pose_est.detect_pose(video,canvas,displaySize,oval);
+        result.then(text_pose => {
+            // Write user action
+            draw_text(canvas,"Detect status: "+ text_pose,{x:10,y:50});
+            // Check condition 
+            if (text_pose.includes(current_request) ) {
+                current_request = getRequest(request_actions,current_request);
+                author_score += 1;
+                if (author_score > FINISH_SOCRE)
+                    author_score = 0;
+            };
+
+            // Write request action
+            draw_text(canvas,"Please "+ current_request,{x:10,y:350});
+            // Write score:
+            if (author_score < FINISH_SOCRE)
+                draw_text(canvas,"Score: " + author_score,{x:10,y:300});
+            else{
+                draw_text(canvas,"PASS!",{x:10,y:300});
+                current_request = request_actions[0];
+            }
+
+
+        });
     },100);
+
+
+
 })
 
